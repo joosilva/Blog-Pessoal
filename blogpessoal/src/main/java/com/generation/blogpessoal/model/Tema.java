63546908cc653232811aa27435fde574447b5e1a
joosilva <joaovictormendessilva1@gmail.com>
2023-03-20 20:14:16 -0300

    Criação e inserção das tabelas no SQL.

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,39 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "Campo obrigatório.")
+	@Size(min = 5, max = 100, message = "O tema deve conter entre 5 e 100 caracteres.")
+	private String tema;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return this.tema;
+	}
+
+	public void setDescricao(String descricao) {
+		this.tema = descricao;
+	}
+
+}
