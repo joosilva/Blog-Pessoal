890d2d975b32031387dcd510793d3f3883591e37
joosilva <joaovictormendessilva1@gmail.com>
2023-03-31 12:27:12 -0300

    Criação da model usuário e criação da relação entre tabelas.

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,16 +4,11 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-<<<<<<< HEAD
-=======
-import jakarta.persistence.CascadeType;
->>>>>>> bbdfeab106f1c5fcf5242f33a8983d16130632e6
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-<<<<<<< HEAD
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -38,44 +33,12 @@ public class Usuario {
 	private String usuario;
 	
 	@NotBlank(message = "Campo obrigatório.")
-=======
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-@Entity
-@Table(name = "tb_usuario")
-public class Usuario {
-	
-	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@NotBlank(message = "Campo obrigatório.")
-	@Size(min = 10, max = 255, message = "O nome deve conter entre 10 e 255 caracteres.")
-	private String nome;
-	
-	@NotBlank(message = "Campo obrigatório.")
-	@Size(min = 5, max = 255, message = "O usuário deve conter entre 5 e 255 caracteres.")
-	private String usuario;
-	
-	
-	@NotBlank(message = "Campo obrigatório.")
-	@Size(min = 10, max = 255, message = "O email deve conter entre 10 e 255 caracteres.")
-	private String email;
-	
-	@NotBlank(message = "O campo senha é obrigatório.")
->>>>>>> bbdfeab106f1c5fcf5242f33a8983d16130632e6
 	@Size(min = 8, max = 16, message = "A senha deve conter entre 8 e 16 caracteres.")
 	private String senha;
 	
 	private String foto;
 	
-<<<<<<< HEAD
 	@OneToMany
-=======
-	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
->>>>>>> bbdfeab106f1c5fcf5242f33a8983d16130632e6
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagens;
 
@@ -95,7 +58,6 @@ public class Usuario {
 		this.nome = nome;
 	}
 
-<<<<<<< HEAD
 	public String getNickname() {
 		return nickname;
 	}
@@ -104,8 +66,6 @@ public class Usuario {
 		this.nickname = nickname;
 	}
 
-=======
->>>>>>> bbdfeab106f1c5fcf5242f33a8983d16130632e6
 	public String getUsuario() {
 		return usuario;
 	}
@@ -114,17 +74,6 @@ public class Usuario {
 		this.usuario = usuario;
 	}
 
-<<<<<<< HEAD
-=======
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
->>>>>>> bbdfeab106f1c5fcf5242f33a8983d16130632e6
 	public String getSenha() {
 		return senha;
 	}
